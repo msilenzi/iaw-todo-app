6ba99065890c2d233941884c5cd141d09e1e0827
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 12:29:01 -0300

    add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  //
+  // Swagger
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo API')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build()
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_, methodKey) => methodKey,
+  }
+
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'Todo API',
+  }
+
+  const document = SwaggerModule.createDocument(app, config, options)
+  SwaggerModule.setup('api', app, document, customOptions)
+
+  //
+  // Listen
+
   await app.listen(3000)
 }
+
 bootstrap()
