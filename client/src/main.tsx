ffc4d7d212f3ac4bfdac2ac0a439e3921496a8c3
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 15:04:15 -0300

    install bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
