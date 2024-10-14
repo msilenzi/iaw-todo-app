bce5a094218a8c961957fd7429b5f948f03280ac
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 16:54:24 -0300

    improve Todo schema

--- server/src/todos/schemas/todo.schema.ts
+++ server/src/todos/schemas/todo.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { HydratedDocument } from 'mongoose'
+import { ApiProperty } from '@nestjs/swagger'
+import { HydratedDocument, Types } from 'mongoose'
 
 export type TodoDocument = HydratedDocument<Todo>
 
@@ -17,6 +18,19 @@ export class Todo {
 
   @Prop({ required: false, default: false })
   done: boolean
+
+  // Campos generados automáticamente por Mongoose:
+  // Estos campos no llevan @Prop porque están para que los detecte Swagger
+  // y genere los tipos correctamente. No influyen en la creación de la BD.
+
+  @ApiProperty({ type: String })
+  _id: Types.ObjectId
+
+  createdAt: Date
+
+  updatedAt: Date
+
+  // __v: number
 }
 
 export const TodoSchema = SchemaFactory.createForClass(Todo)
