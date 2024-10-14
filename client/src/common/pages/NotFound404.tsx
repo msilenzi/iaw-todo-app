8ff69384af387b342788b276b6bcd40b9cf63ec1
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 16:04:08 -0300

    add static content to pages

--- client/src/common/pages/NotFound404.tsx
+++ client/src/common/pages/NotFound404.tsx
@@ -1,3 +1,46 @@
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
 export default function NotFound404() {
-  return <div>NotFound404</div>
+  return (
+    <div className="min-vh-100 d-flex flex-column">
+      <Navbar expand="lg" className="bg-body-tertiary mb-5">
+        <Container>
+          <Navbar.Brand href="#home">Todo App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" active>
+                Todos
+              </Nav.Link>
+              <Nav.Link href="#link" active={false}>
+                About
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="flex-grow-1 mb-5">
+        <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-5">
+          <div>
+            <h2 className="fs-1">Página no encontrada</h2>
+            <p className="mb-4">Lo sentimos, no pudimos encontrar la página</p>
+            <Button>Volver al inicio</Button>
+          </div>
+          <div className="text-secondary" style={{ fontSize: '8rem' }}>
+            <i className="bi bi-patch-question"></i>
+          </div>
+        </div>
+      </Container>
+
+      <footer>
+        <span className="text-center text-muted d-inline-block w-100 py-2">
+          Copyright &copy; 2024
+        </span>
+      </footer>
+    </div>
+  )
 }
--- client/src/features/todos/pages/TodoPage.tsx
+++ client/src/features/todos/pages/TodoPage.tsx
@@ -1,3 +1,91 @@
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+
 export default function TodoPage() {
-  return <div>TodoPage</div>
+  const date = new Intl.DateTimeFormat('es-ES', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(new Date())
+
+  return (
+    <div className="min-vh-100 d-flex flex-column">
+      <Navbar expand="lg" className="bg-body-tertiary mb-5">
+        <Container>
+          <Navbar.Brand href="#home">Todo App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" active>
+                Todos
+              </Nav.Link>
+              <Nav.Link href="#link" active={false}>
+                About
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="flex-grow-1 mb-5">
+        <div className="d-flex justify-content-between flex-column flex-md-row">
+          <h2 className="m0">Comprar Pan</h2>
+          <div className="d-flex gap-2">
+            <Button variant="primary">
+              <i className="bi bi-pencil-fill"></i>
+            </Button>
+            <Button variant="danger">
+              <i className="bi bi-trash-fill"></i>
+            </Button>
+          </div>
+        </div>
+
+        <p className="fw-light text-muted mb-4">Creado el {date}</p>
+
+        <ToggleButtonGroup
+          className="mb-5"
+          type="radio"
+          name="options"
+          value="sin-terminar"
+        >
+          <ToggleButton
+            id="sin-terminar"
+            value="sin-terminar"
+            variant="outline-primary"
+          >
+            Sin Terminar
+          </ToggleButton>
+          <ToggleButton
+            id="terminado"
+            value="terminado"
+            variant="outline-primary"
+          >
+            Terminado
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <section>
+          <h4>Descripción</h4>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            consequuntur natus consequatur minima nesciunt possimus, fugit
+            fugiat debitis, officiis rerum expedita, nemo aliquam asperiores
+            harum. Dolorum suscipit atque molestias earum!
+          </p>
+        </section>
+      </Container>
+
+      <footer>
+        <span className="text-center text-muted d-inline-block w-100 py-2">
+          Copyright &copy; 2024
+        </span>
+      </footer>
+    </div>
+  )
 }
