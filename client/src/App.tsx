feabeb0b09ac740238a6270abbd83f0c530875e6
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 15:00:52 -0300

    add routes and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import { Navigate, Route, Routes } from 'react-router-dom'
+import NotFound404 from './common/pages/NotFound404'
+import AboutPage from './features/about/pages/AboutPage'
+import TodoPage from './features/todos/pages/TodoPage'
+import TodosPage from './features/todos/pages/TodosPage'
+
+export default function App() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/todos" />} />
+      <Route path="/todos/" element={<TodosPage />} />
+      <Route path="/todos/:id" element={<TodoPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="*" element={<NotFound404 />} />
+    </Routes>
   )
 }
-
-export default App
