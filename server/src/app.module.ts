348437944895741ac76ed5a1130ec478f4a50ea9
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 12:50:48 -0300

    generate todos resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { TodosModule } from './todos/todos.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/todo-api')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/todo-api'), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
