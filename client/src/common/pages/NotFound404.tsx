76bc530f350147f4869efe24d7cdca3b40c28009
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-14 16:43:30 -0300

    NotFound404 vuelve al inicio

--- client/src/common/pages/NotFound404.tsx
+++ client/src/common/pages/NotFound404.tsx
@@ -1,12 +1,15 @@
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
 
 export default function NotFound404() {
+  const navigate = useNavigate()
+
   return (
     <div className="d-flex flex-column flex-md-row justify-content-center align-items-center gap-5">
       <div>
         <h2 className="fs-1">Página no encontrada</h2>
         <p className="mb-4">Lo sentimos, no pudimos encontrar la página</p>
-        <Button>Volver al inicio</Button>
+        <Button onClick={() => navigate('/')}>Volver al inicio</Button>
       </div>
       <div className="text-secondary" style={{ fontSize: '8rem' }}>
         <i className="bi bi-patch-question"></i>
