b7dd83542ce8d4385f5395f9dbb2aa763f1c883c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-10-16 00:43:12 -0300

    add 'No se encontraron Todos'

--- client/src/features/todos/pages/TodosPage.tsx
+++ client/src/features/todos/pages/TodosPage.tsx
@@ -5,6 +5,7 @@ import { CreateTodoDto, Todo } from '../../../common/api/generated'
 import Loading from '../../../common/components/Loading'
 import CreateUpdateTodoModal from '../components/CreateUpdateTodoModal'
 import TodosList from '../components/TodosList'
+import CtaBanner from '../../../common/components/CtaBanner'
 
 export default function TodosPage() {
   const [todos, setTodos] = useState<Todo[]>([])
@@ -32,12 +33,23 @@ export default function TodosPage() {
 
   return (
     <>
-      <div className="d-flex align-items-center justify-content-between">
-        <h2 className="mb-4">Todos</h2>
+      <div className="d-flex align-items-center justify-content-between mb-4">
+        <h2>Todos</h2>
         <Button onClick={() => setIsCreateVisible(true)}>
           <i className="bi bi-plus-lg"></i> Agregar Todo
         </Button>
       </div>
+      {todos.length === 0 && (
+        <div className="mt-5">
+          <CtaBanner
+            title="No se encontraron Todos"
+            description="Parece que todavía no tenés Todos creados. No te preocupes, podés crear uno ahora."
+            ctaText="Crear nuevo Todo"
+            icon="bi bi-inbox"
+            onCtaClick={() => setIsCreateVisible(true)}
+          />
+        </div>
+      )}
       <TodosList todos={todos} />
       <CreateUpdateTodoModal
         isVisible={isCreateVisible}
