2b46b61d81c71c0bfaecb6b75a89f23c984ebff9
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-04 09:26:50 -0300

    add auth0 in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,23 @@ import AboutPage from './features/about/pages/AboutPage'
 import TodoPage from './features/todos/pages/TodoPage'
 import TodosPage from './features/todos/pages/TodosPage'
 import MainLayout from './common/layout/MainLayout'
+import { useAuth0 } from '@auth0/auth0-react'
+import Loading from './common/components/Loading'
+import AuthPage from './features/auth/pages/AuthPage'
 
 export default function App() {
+  const { isLoading, isAuthenticated } = useAuth0()
+
+  console.log({ isLoading, isAuthenticated })
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (!isAuthenticated) {
+    return <AuthPage />
+  }
+
   return (
     <MainLayout>
       <Routes>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,20 @@ import App from './App.tsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-6wju6g4dd47pqwzg.us.auth0.com"
+      clientId="fXPGvPFBMo5NJ3g8ch4T4Ku7p9msyUi8"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </StrictMode>
 )
